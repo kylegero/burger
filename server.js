92afc8b2294283fe75124778e6d43fe75aeaf55b
Kyle Pykonen <kylepyk@gmail.com>
2021-01-02 21:43:59 -0800

    fixed some  typos in server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-var initialcommit = "here"
+var express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const PORT = process.env.PORT || 3131;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+

e90749c2776301b32ccb58434d6610a74b772d96
Kyle Pykonen <kylepyk@gmail.com>
2021-01-02 21:25:34 -0800

    Adding code to burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,23 @@ const burger = require("../models/burger.js")
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        let picked = [];
+        let eaten =[];
+        data.forEach((burger) => {
+            if (burger.devoured) {
+                eaten.push(burger);
+            } else {
+                picked.push(burger);
+            }
+        });
+        let burgerObj = {
+            burgers_picked: picked,
+            burgers_eaten: eaten
+        };
+        res.render("index", burgerObj);
+    })
+});
+
 module.exports = router;

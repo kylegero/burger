aaa64bfb215bc248933236f548c98810609d41bd
Kyle Pykonen <kylepyk@gmail.com>
2021-01-02 16:04:09 -0800

    added selectAll method to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,13 @@
 const connection = require("./connection.js")
 
+let orm = {
+    selectAll: (table, cb) => {
+        let query = `SELECT * FROM ??`;
+        connection.query(query, table, (err, res) => {
+            if (err) throw err;
+            cb(res);
+        });
+    }
+}
 
 module.exports = orm;

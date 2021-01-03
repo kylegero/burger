e2c8a4d551cccd1375db2ccd20776ae36f0c1cff
Kyle Pykonen <kylepyk@gmail.com>
2021-01-02 16:13:48 -0800

    added code to call selectAll and insertOne

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,14 @@
 const orm = require ("../config/orm.js")
+
+const burgers = {
+    selectAll: (cb) => {
+        orm.selectAll("burgers", (res) => {
+            cb(res);
+        });
+    },
+    insertOne: (newName, cb) => {
+        orm.insertOne("burgers", "burger_name", newName, (res) => {
+            cb(res);
+        });
+    }
+}
